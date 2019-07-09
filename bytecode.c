82e432bca251f97de7fea9822721db8f088fd84d
Beaux <ominouspotato.43@gmail.com>
2019-07-09 16:05:40 -0500

    Added cmp and test functions, idk what test is for, but it was on the wiki so I added it

--- bytecode.c
+++ bytecode.c
@@ -7,7 +7,15 @@
 #define SF (flags.sf)
 #define ZF (flags.zf)
 #define OF (flags.of)
+#define EQ() (ZF)
+#define NE() (!ZF)
+#define LT() ((SF ^ OF) && NE())
+#define LE() ((SF ^ OF) || EQ())
+#define GT() (!(SF ^ OF) && NE())
+#define GE() (!(SF ^ OF) || EQ())
 
+void test(short *, short *);
+void cmp(short *, short *);
 struct {
   bool sf: 1;
   bool zf: 1;
@@ -30,6 +38,26 @@ int main(int argc, char **argv) {
   char  *const DL = ((char*) DX) + O32_IS_BIG;
   *AX = 0x6F3F;
   *BX = 0x6F3F;
+  cmp(AX, BX);
+  printf("AX:%X,BX:%X\n", *AX, *BX);
+  printf("SF:%d, ZF:%d, OF:%d\n", SF, ZF, OF);
+  printf("GT:%d\nGE:%d\nLT:%d\nLE:%d\nEQ:%d\nNE:%d\n", GT(), GE(), LT(), LE(), EQ(), NE());
 
   return 0;
+}
+
+void test(short *arg1, short *arg0) {
+  short commonBits = *arg1 & *arg0;
+  SF = commonBits & 0x8000;
+  ZF = !commonBits;
+  OF = false;
+}
+
+void cmp(short *minuend, short *subtrahend) {
+  short a = *minuend,
+        b = *subtrahend,
+        difference = a - b;
+  SF = difference & 0x8000;
+  ZF = !difference;
+  OF = (a > 0 && b > SHRT_MAX - a) || (a < 0 && b < SHRT_MIN - a);
 }
