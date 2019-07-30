de642f6b5f882f758ece4a69d7f7f4e5e5ba0d93
Beaux <ominouspotato.43@gmail.com>
2019-07-30 11:14:22 -0500

    Added 32-bit registers, made it work, and added comments

--- bytecode.c
+++ bytecode.c
@@ -7,15 +7,20 @@
 #define SF (flags.sf)
 #define ZF (flags.zf)
 #define OF (flags.of)
+#define CF (flags.of)
 #define EQ() (ZF)
 #define NE() (!ZF)
 #define LT() ((SF ^ OF) && NE())
 #define LE() ((SF ^ OF) || EQ())
 #define GT() (!(SF ^ OF) && NE())
 #define GE() (!(SF ^ OF) || EQ())
+#define AB() (!(CF || EQ()))
+#define AE() (!CF && EQ())
+#define BL() (CF)
+#define BE() (CF || EQ())
 
-void test(short *, short *);
-void cmp(short *, short *);
+void test(unsigned *, unsigned *);
+void cmp(unsigned *, unsigned *);
 struct {
   bool sf: 1;
   bool zf: 1;
@@ -23,47 +28,98 @@ struct {
 } flags = {0};
 
 int main(int argc, char **argv) {
-  short *const REGS = calloc(9, sizeof(short));
-  short *const AX = REGS + 0,
-        *const BX = REGS + 1,
-        *const CX = REGS + 2,
-        *const DX = REGS + 3,
-        *const SP = REGS + 4,
-        *const BP = REGS + 5,
-        *const IP = REGS + 6,
-        *const DI = REGS + 7,
-        *const SI = REGS + 8;
-  
-  char  *const AH = ((char*) AX) + O32_IS_LITTLE;
-  char  *const AL = ((char*) AX) + O32_IS_BIG;
-  char  *const BH = ((char*) BX) + O32_IS_LITTLE;
-  char  *const BL = ((char*) BX) + O32_IS_BIG;
-  char  *const CH = ((char*) CX) + O32_IS_LITTLE;
-  char  *const CL = ((char*) CX) + O32_IS_BIG;
-  char  *const DH = ((char*) DX) + O32_IS_LITTLE;
-  char  *const DL = ((char*) DX) + O32_IS_BIG;
-  *AX = 0x6F3F;
-  *BX = 0x6F3F;
-  cmp(AX, BX);
-  printf("AX:%X,BX:%X\n", *AX, *BX);
-  printf("SF:%d, ZF:%d, OF:%d\n", SF, ZF, OF);
-  printf("GT:%d\nGE:%d\nLT:%d\nLE:%d\nEQ:%d\nNE:%d\n", GT(), GE(), LT(), LE(), EQ(), NE());
+  // REGISTERS
+  unsigned *const REGS = calloc(9, sizeof(unsigned));
+  if(!REGS) {
+    puts("wtf");
+    return -1;
+  }
+
+  unsigned *const EAX = REGS + 0,   // Accumulator register
+           *const EBX = REGS + 1,   // Base register
+           *const ECX = REGS + 2,   // Counter register
+           *const EDX = REGS + 3,   // Data register
+           *const ESP = REGS + 4,   // Stack pointer
+           *const EBP = REGS + 5,   // Base pointer
+           *const EIP = REGS + 6,   // Instruction pointer
+           *const EDI = REGS + 7,   // Destination Index
+           *const ESI = REGS + 8;   // Source Index
+
+  /*
+    This may be excessively long winded for what it's attempting to get across, of which I'm unsure.
+
+    x86 processors have two 8-bit registers corresponding to the high and low bits of a 16-bit register
+    that is the least significant 16-bits of a 32-bit register for all the integer registers.
+
+    O32_IS_LITTLE and O32_IS_BIG are macros that determine whether the host machine is little or big
+    endian; as endianness is an important factor since the high registers must be the high bits, and the
+    low registers must be the low bits.
+  */
+  unsigned short *const AX = ((unsigned short*) EAX) + O32_IS_BIG,
+                 *const BX = ((unsigned short*) EBX) + O32_IS_BIG,
+                 *const CX = ((unsigned short*) ECX) + O32_IS_BIG,
+                 *const DX = ((unsigned short*) EDX) + O32_IS_BIG,
+                 *const SP = ((unsigned short*) ESP) + O32_IS_BIG,
+                 *const BP = ((unsigned short*) EBP) + O32_IS_BIG,
+                 *const IP = ((unsigned short*) EIP) + O32_IS_BIG,
+                 *const DI = ((unsigned short*) EDI) + O32_IS_BIG,
+                 *const SI = ((unsigned short*) ESI) + O32_IS_BIG;
+
+  unsigned char *const AH = ((unsigned char*) AX) + O32_IS_LITTLE,
+                *const AL = ((unsigned char*) AX) + O32_IS_BIG,
+                *const BH = ((unsigned char*) BX) + O32_IS_LITTLE,
+                *const BL = ((unsigned char*) BX) + O32_IS_BIG,
+                *const CH = ((unsigned char*) CX) + O32_IS_LITTLE,
+                *const CL = ((unsigned char*) CX) + O32_IS_BIG,
+                *const DH = ((unsigned char*) DX) + O32_IS_LITTLE,
+                *const DL = ((unsigned char*) DX) + O32_IS_BIG;
+
+  // Note that the content of the code segment is intentionally not const, as there were no
+  // restrictions employed by the hardware on a program that disallowed it from editing its
+  // own code, and that it's a char* rather than a short* so that it has single byte granularity
+  char  *const CS = malloc(1024*64 * sizeof(short)); // Code segment
+  short *const SS = malloc(1024*64 * sizeof(short)), // Stack segment
+        *const DS = malloc(1024*64 * sizeof(short)), // Data segment
+        *const ES = malloc(1024*64 * sizeof(short)); // Extra segment
+
+  // Read code into CS
+  if(argc != 2) {
+    printf("Takes exactly 1 argument, got %d.\n", argc - 1);
+    return -1;
+  }
+
+  char *fileName = NULL;
+  if(argc > 1) {
+    fileName = argv[1];
+    long length;
+    FILE *fp = fopen(fileName, "rb");
+
+    if(fp) {
+      fseek(fp, 0, SEEK_END);
+      length = ftell(fp);
+      if(length) {
+        fread(CS, 1, length, fp);
+      }
+      fclose(fp);
+    }
+  }
 
   return 0;
 }
 
-void test(short *arg1, short *arg0) {
-  short commonBits = *arg1 & *arg0;
-  SF = commonBits & 0x8000;
+
+void test(unsigned *arg1, unsigned *arg0) {
+  unsigned commonBits = *arg1 & *arg0;
+  SF = commonBits & 0x80000000;
   ZF = !commonBits;
   OF = false;
 }
 
-void cmp(short *minuend, short *subtrahend) {
-  short a = *minuend,
-        b = *subtrahend,
-        difference = a - b;
-  SF = difference & 0x8000;
+void cmp(unsigned *minuend, unsigned *subtrahend) {
+  unsigned a = *minuend,
+           b = *subtrahend,
+           difference = a - b;
+  SF = difference & 0x80000000;
   ZF = !difference;
   OF = (a > 0 && b > SHRT_MAX - a) || (a < 0 && b < SHRT_MIN - a);
 }
